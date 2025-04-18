94287dbf2a459859bc023f6d0a86424dc34b17ce
Matteo Bambini <matteo.bambini00@gmail.com>
2025-04-18 18:54:08 +0200

    Completed RidesMap interface

--- frontend/src/views/RidesMap.vue
+++ frontend/src/views/RidesMap.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { ref, onMounted, computed } from 'vue';
 import { LMap, LTileLayer, LControlZoom, LMarker, LIcon } from "@vue-leaflet/vue-leaflet";
-import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+import { faEye, faEyeSlash, faArrowRightArrowLeft, faBus, faRoute, faClock } from '@fortawesome/free-solid-svg-icons';
 import { BusRideService } from '@/service/BusRideService';
 import { WebSocket } from '@/lib/websocket'
 
@@ -15,11 +15,18 @@ const mapCenter = ref([44.136352, 12.242244])
 const mapZoom = ref(13)
 const rides = ref([])
 const realTimeData = ref({})
+const hiddenKeys = ref(new Set())
+const expandedKeys = ref({})
 const socket = new WebSocket('/rides')
 
 const convertRidesToLines = () => {
     const lines = {}
-    rides.value.forEach(ride => {
+    rides.value.filter(ride => {
+        if (selectButtonValue.value.value == 0) {
+            return true
+        }
+        return realTimeData.value[ride.id] && realTimeData.value[ride.id].minutesLate > 0;
+    }).forEach(ride => {
         if (!lines[ride.line.id]) {
             lines[ride.line.id] = {
                 name: ride.line.name,
@@ -32,14 +39,17 @@ const convertRidesToLines = () => {
                 rides: {}
             }
         }
-        lines[ride.line.id].directions[ride.line.direction.id].rides[ride.id] = {}
+        lines[ride.line.id].directions[ride.line.direction.id].rides[ride.id] = {
+            scheduledDepartureTimestamp: ride.scheduledDepartureTimestamp,
+            minutesLate: realTimeData.value[ride.id] ? realTimeData.value[ride.id].minutesLate : undefined
+        }
     })
     return lines
 }
 
 const updateRealTimeData = (ridesData) => {
     ridesData.forEach(ride => {
-        realTimeData.value[ride.rideId] = ride
+        realTimeData.value[ride.rideId] = ride;
     });
 }
 
@@ -47,18 +57,28 @@ const linesNodes = computed(() => Object.entries(convertRidesToLines()).map(([li
     return {
         key: lineId,
         label: line.name,
+        icon: faRoute,
         children: Object.entries(line.directions).map(([dirId, dir]) => {
             return {
                 key: dirId,
                 label: dir.name,
-                children: Object.entries(dir.rides).map(([rideId, ride]) => ({key: rideId, label: rideId}))
+                icon: faArrowRightArrowLeft,
+                children: Object.entries(dir.rides).map(([rideId, ride]) => {
+                    const time = new Date(ride.scheduledDepartureTimestamp)
+                    return {
+                        key: rideId,
+                        label: `${time.getHours().toString().padStart(2, '0')}:${time.getMinutes().toString().padStart(2, '0')}`,
+                        minutesLate: ride.minutesLate,
+                        icon: faBus
+                    };
+                })
             }
         })
-    }
+    };
 }))
 
 const ridesPositions = computed(() => rides.value.map(ride => {
-    if (!realTimeData.value[ride.id]) {
+    if (!realTimeData.value[ride.id] || hiddenKeys.value.has(ride.id)) {
         return;
     }
     return {
@@ -70,21 +90,49 @@ const ridesPositions = computed(() => rides.value.map(ride => {
 
 const retrieveRidesList = async () => {
     try {
-        rides.value = await BusRideService.getBusRidesList()
-        socket.emit('subscribe', rides.value.map(ride => ride.id))
+        rides.value = await BusRideService.getBusRidesList();
+        socket.emit('subscribe', rides.value.map(ride => ride.id));
     } catch {
-        rides.value = []
+        rides.value = [];
     }
 }
 
 onMounted(async () => {
     await retrieveRidesList()
     socket.emit('get-rides', rides.value.map(ride => ride.id), (ridesData) => {
-        updateRealTimeData(ridesData)
+        updateRealTimeData(ridesData);
     })
-    socket.on('update', data => updateRealTimeData([data]))
-    setInterval(retrieveRidesList, 5000)
+    socket.on('update', data => updateRealTimeData([data]));
+    setInterval(retrieveRidesList, 5000);
 })
+
+const hide = (node) => {
+    hiddenKeys.value.add(node.key);
+    if (!node.children) {
+        return;
+    }
+    node.children.forEach(ch => hide(ch));
+}
+
+const show = (node) => {
+    hiddenKeys.value.delete(node.key)
+    if (!node.children) {
+        return;
+    }
+    node.children.forEach(ch => show(ch));
+}
+
+const hideAll = () => {
+    linesNodes.value.forEach(hide);
+}
+
+const expand = (node) => {
+    if (!node.children) {
+        return;
+    }
+    expandedKeys.value[node.key] = true;
+    node.children.forEach(expand);
+}
 
 </script>
 <template>
@@ -111,17 +159,46 @@ onMounted(async () => {
         <div class="col-span-6 md:col-span-3 flex flex-col items-center">
             <h2 class="text-xl mt-3">Corse Attive</h2>
             <SelectButton v-model="selectButtonValue" :options="selectButtonOptions" optionLabel="name" class="mt-2" />
-            <div class="text-right w-full">
-                <Button label="Nascondi tutti" severity="info" variant="text" />
+            <div class="text-right w-full mt-2 flex flex-row content-center justify-between">
+                <div>
+                    <Button v-if="Object.keys(expandedKeys).length == 0" label="Espandi tutti" severity="info" variant="text" @click="_ => linesNodes.forEach(expand)" />
+                    <Button v-else label="Collassa tutti" severity="info" variant="text" @click="_ => expandedKeys = {}" />
+                </div>
+                <div>
+                    <Button v-if="hiddenKeys.size == 0" label="Nascondi tutti" severity="info" variant="text" @click="_ => hideAll()" />
+                    <Button v-else label="Mostra tutti" severity="info" variant="text" @click="_ => hiddenKeys.clear()" />
+                </div>
             </div>
-            <Tree class="w-full" :value="linesNodes">
-                <template #icon="scope">
-                    <font-awesome-icon v-if="scope.node.icon" :icon="scope.node.icon" />
+            <Tree class="w-full" :value="linesNodes"
+                v-model:expanded-keys="expandedKeys">
+                <template #nodeicon="scope">
+                    <font-awesome-icon :icon="scope.node.icon" />
                 </template>
-                <template #checkboxicon="scope">
-                    <font-awesome-icon :icon="scope.checked ? faEye : faEyeSlash" />
+                <template #default="scope">
+                    <div class="flex flex-row items-center justify-between w-full">
+                        <div>
+                            <span v-if="scope.node.children">{{ scope.node.label }}</span>
+                            <span v-else>{{ scope.node.label }}</span>
+                        </div>
+                        <div v-if="!scope.node.children && scope.node.minutesLate">
+                            <font-awesome-icon class="text-green-600" v-if="scope.node.minutesLate == 0" :icon="faClock" />
+                            <span class="font-bold text-red-600">+{{ scope.node.minutesLate }}</span>
+                        </div>
+                        <Button v-if="hiddenKeys.has(scope.node.key)" severity="contrast" variant="text" rounded aria-label="Mostra" @click="_ => show(scope.node)">
+                            <font-awesome-icon :icon="faEyeSlash" />
+                        </Button>
+                        <Button v-else severity="contrast" variant="text" rounded aria-label="Nascondi" @click="_ => hide(scope.node)">
+                            <font-awesome-icon :icon="faEye" />
+                        </Button>
+                    </div>
                 </template>
             </Tree>
         </div>
     </div>
 </template>
+<style>
+.p-tree-node-label {
+    width: 100% !important;
+}
+</style>
+
